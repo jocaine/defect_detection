9469b0047e62779aab85a7ceecf9bfe6a5d4532f
滕乐然 <2508087548@qq.com>
2025-05-25 16:58:04 +0800

    取消构建文件的追踪

--- build/unknown-RelWithDebInfo/CMakeFiles/linux_demo.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for linux_demo.
-# This may be replaced when dependencies are built.
--- build/unknown-RelWithDebInfo/CMakeFiles/linux_demo.dir/resultcollector.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/linux_demo.dir/resultcollector.cpp.o: \
- /home/tlr/projects/linux_demo/resultcollector.cpp \
- /usr/include/stdc-predef.h \
- /home/tlr/projects/linux_demo/resultcollector.h
